6ce60bb77fede70f497ebbfe89ce2805c135a515
Surya <suryanshsingh9736@gmail.com>
2025-09-20 12:28:15 +0530

    pricvate folders

---
+++ app/_lib/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
+// _lib creates a private folder so we netx js do not make roues for file under it
